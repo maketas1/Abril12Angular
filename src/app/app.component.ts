31dbe851ebf2c45e4bcda2aca5947dc3cd1da9b8
Ángel Gallardo <angelgallardoturpin@gmail.com>
2024-04-12 12:41:54 +0200

    ejercicio 2 de interpolación y two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TiposDeDatosComponent } from './tipos-de-datos/tipos-de-datos.component';
 import { Ejercicio1Component } from './ejercicio1/ejercicio1.component';
+import { Ejercicio2Component } from './ejercicio2/ejercicio2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TiposDeDatosComponent, Ejercicio1Component],
+  imports: [RouterOutlet, TiposDeDatosComponent, Ejercicio1Component, Ejercicio2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/ejercicio2/ejercicio2.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-ejercicio2',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './ejercicio2.component.html',
+  styleUrl: './ejercicio2.component.css'
+})
+export class Ejercicio2Component {
+  cantidad: number = 0;
+
+  aEurios() {
+    //moneda local por defecto
+    let resultado: number = this.cantidad * 1.0;
+    console.log(resultado);
+  }
+
+  aDolares() {
+    let resultado: number = this.cantidad * 0.85;
+    console.log(resultado);
+  }
+
+  aLibras() {
+    let resultado: number = this.cantidad * 1.07;
+    console.log(resultado);
+  }
+}
